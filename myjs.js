89b58a53148f7920b7889403ce7d17d3268b051f
cwh47 <cwh47@miami.edu>
2017-04-27 18:05:45 -0400

    Create json file

---
+++ myjs.js
@@ -0,0 +1,6 @@
+var saguaro= document.getElementById("saguaro");
+
+saguaro.addEventListener("click", function(){
+    console.log("howdy!");
+    document.getElementById("test").style.display= "block";
+});
