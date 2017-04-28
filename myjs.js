2878bb6dd4aac626baf60505fe39154cdfeee4bf
cwh47 <cwh47@miami.edu>
2017-04-27 21:26:23 -0400

    Changes to json

--- myjs.js
+++ myjs.js
@@ -1,6 +1,21 @@
-var saguaro= document.getElementById("saguaro");
+ var npData = [{
+    "name" : "Laura",
+    "question1" : "Hi I like parks",
+    "question2" : "Me too"
+},
+ {
+    "name" : "Rori",
+    "question1" : "She likes parks",
+    "question2" : "So do I"
+}];
 
-saguaro.addEventListener("click", function(){
+console.log(npData[1].name);
+
+var saguaro = document.getElementById("saguaro");
+
+saguaro.addEventListener("click", function () {
     console.log("howdy!");
-    document.getElementById("test").style.display= "block";
+    document.getElementById("test").style.display = "block";
+    document.getElementById("name").innerHTML = npData[1].name;
 });
+
