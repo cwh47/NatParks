9d2b4c686f8e7bd0325809fcaea85e02ac84cf61
cwh47 <cwh47@miami.edu>
2017-04-28 16:32:55 -0400

    Honestly wtf

--- myjs.js
+++ myjs.js
@@ -1,16 +1,18 @@
-$.getJSON("npData.json", function(data){
-    console.log(data)
+$.ajax({
+    type: "GET"
+    , dataType: "json"
+    , url: "/server/npData.json"
+    , cache: false
+    , success: function (data) {
+        console.log(data);
+    }
 });
-
 var saguaro = document.getElementById("saguaro");
-
 saguaro.addEventListener("click", function () {
     console.log("howdy!");
-    document.getElementById("name").style.display = "block";
-    document.getElementById("name").innerHTML = npData[0].name;
+    document.getElementById("saguaro").style.display = "block";
+    document.getElementById("saguaro").innerHTML = [0].name;
 });
-
-
 /*var modal = document.getElementById("npModal");
 var click = document.getElementById("npClick");
 var span = document.getElementsByClassName("class")[0];
@@ -25,4 +27,3 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }*/
-
