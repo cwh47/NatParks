1867131fcb071172a107518c2c7a3d754884c7fd
cwh47 <cwh47@miami.edu>
2017-04-28 15:59:28 -0400

    Ugh

--- myjs.js
+++ myjs.js
@@ -1,21 +1,28 @@
- var npData = [{
-    "name" : "Laura",
-    "question1" : "Hi I like parks",
-    "question2" : "Me too"
-},
- {
-    "name" : "Rori",
-    "question1" : "She likes parks",
-    "question2" : "So do I"
-}];
-
-console.log(npData[1].name);
+$.getJSON("npData.json", function(data){
+    console.log(data)
+});
 
 var saguaro = document.getElementById("saguaro");
 
 saguaro.addEventListener("click", function () {
     console.log("howdy!");
-    document.getElementById("test").style.display = "block";
+    document.getElementById("name").style.display = "block";
     document.getElementById("name").innerHTML = npData[0].name;
 });
+
+
+/*var modal = document.getElementById("npModal");
+var click = document.getElementById("npClick");
+var span = document.getElementsByClassName("class")[0];
+click.onclick = function () {
+    modal.style.display = "block";
+}
+span.onclick = function () {
+    modal.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}*/
 
