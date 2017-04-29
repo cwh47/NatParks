d745331ac3fdcd5c8b8bed284c97cc4417c208a7
cwh47 <cwh47@miami.edu>
2017-04-29 19:41:02 -0400

    Hover effects

--- myjs.js
+++ myjs.js
@@ -7,23 +7,16 @@ $.ajax({
         console.log(data);
     }
 });
+
+
 var saguaro = document.getElementById("saguaro");
 saguaro.addEventListener("click", function () {
     console.log("howdy!");
-    document.getElementById("saguaro").style.display = "block";
-    document.getElementById("saguaro").innerHTML = [0].name;
+    document.getElementById("saguaro").style.display = "block";;
 });
-/*var modal = document.getElementById("npModal");
-var click = document.getElementById("npClick");
-var span = document.getElementsByClassName("class")[0];
-click.onclick = function () {
-    modal.style.display = "block";
-}
-span.onclick = function () {
-    modal.style.display = "none";
-}
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}*/
+
+var haleakala = document.getElementById("haleakala");
+haleakala.addEventListener("click", function () {
+    console.log("howdy!");
+});
+
