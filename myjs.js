54bf45de6ebbc95edc3abb88ab5db260638bafd1
cwh47 <cwh47@miami.edu>
2017-05-01 15:00:13 -0400

    Typos and added JS

--- myjs.js
+++ myjs.js
@@ -8,16 +8,14 @@ $.ajax({
         console.log(data);
     }
 });
+
 //display block
 var saguaro = document.getElementById("saguaro");
 saguaro.addEventListener("click", function () {
     console.log("howdy!");
-    document.getElementById("saguaro").style.display = "block";;
+    document.getElementById("test").style.display = "block";
 });
-var haleakala = document.getElementById("haleakala");
-haleakala.addEventListener("click", function () {
-    console.log("howdy!");
-});
+
 //saguaro hover
 $("#saguaro").on("mouseover", function () {
     console.log("woof");
@@ -27,7 +25,6 @@ $("#saguaro").on("mouseout", function () {
     console.log("meow");
     $("#saguaroLines").css('display', 'none');
 });
-
 //haleakala hover
 $("#haleakala").on("mouseover", function () {
     $("#haleakalaLines").css('display', 'inline');
@@ -36,7 +33,6 @@ $("#haleakala").on("mouseout", function () {
     console.log("meow");
     $("#haleakalaLines").css('display', 'none');
 });
-
 //everglades hover
 $("#everglades").on("mouseover", function () {
     $("#evergladesLines").css('display', 'inline');
